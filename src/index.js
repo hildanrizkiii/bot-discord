a9faca64b9b8bf6ec13659b6c265fa2d61b5aeec
hildanrizkiii <linuxhildan@gmail.com>
2025-03-27 11:50:11 +0700

    update commands

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
-const { Client, IntentsBitField} = require('discord.js');
-const { dotenv } = require('dotenv').config();
+const { Client, IntentsBitField ,ActivityType} = require('discord.js');
+require('dotenv').config();
+const registerCommands = require('./register-commands');
+
+
 const client = new Client({
     intents: [
         IntentsBitField.Flags.Guilds,
@@ -9,8 +12,36 @@ const client = new Client({
     ]
 });
 
+client.on('messageCreate', (message) => {
+    if(message.content === 'ping') {
+        message.reply('pong');
+    }
+})
+client.on('interactionCreate', (interaction) => {
+    if(interaction.isChatInputCommand()) {
+        if(interaction.commandName === 'ping') {
+            interaction.reply('pong');
+        }
+    }
+    if(interaction.isChatInputCommand()) {
+        if(interaction.commandName === 'bio') {
+            interaction.reply('BIO KONTOLLLLLLL');
+        }
+    }
+    if(interaction.isChatInputCommand()) {
+        if(interaction.commandName === 'sahur') {
+            interaction.reply('tung tung tung sahur');
+        }
+    }
+})
 
 client.login(process.env.TOKEN_DISCORD);
 client.on('ready', (c) => {
     console.log(`Logged in as ${c.user.tag}!`);
+
+    client.user.setActivity({
+        name: 'Hildan',
+        type: ActivityType.Streaming,
+        url: 'https://www.youtube.com/live/9UV8UHN3l5U?si=t4tJVpNHBhjH9jPF'
+    });
 });
